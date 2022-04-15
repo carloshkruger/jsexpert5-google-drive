6f0d43303bb600c156608b4b5645f402d4b54fcd
carloshkruger <carloshkruger@gmail.com>
2022-04-15 16:43:41 -0300

    get returning file status

--- aulas/aula01/gdrive-webapi/src/routes.js
+++ aulas/aula01/gdrive-webapi/src/routes.js
@@ -1,7 +1,17 @@
+import FileHelper from "./fileHelper.js";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const defaultDownloadsFolder = resolve(__dirname, "../", "downloads");
+
 export default class Routes {
   io;
 
-  constructor() {}
+  constructor(downloadsFolder = defaultDownloadsFolder) {
+    this.downloadsFolder = downloadsFolder;
+    this.fileHelper = FileHelper;
+  }
 
   setSocketInstance(io) {
     this.io = io;
@@ -21,10 +31,13 @@ export default class Routes {
   }
 
   async get(request, response) {
-    response.end();
+    const files = await this.fileHelper.getFilesStatus(this.downloadsFolder);
+
+    response.writeHead(200);
+    response.end(JSON.stringify(files));
   }
 
-  handler(request, response) {
+  async handler(request, response) {
     response.setHeader("Access-Control-Allow-Origin", "*");
 
     const chosen = this[request.method.toLowerCase()] || this.defaultRoute;
--- aulas/aula01/gdrive-webapi/test/unit/routes.test.js
+++ aulas/aula01/gdrive-webapi/test/unit/routes.test.js
@@ -2,6 +2,21 @@ import { describe, test, expect, jest } from "@jest/globals";
 import Routes from "../../src/routes.js";
 
 describe("Routes", () => {
+  const defaultParams = {
+    request: {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      method: "",
+      body: {},
+    },
+    response: {
+      setHeader: jest.fn(),
+      writeHead: jest.fn(),
+      end: jest.fn(),
+    },
+  };
+
   describe("setSocketInstance", () => {
     test("setSocketInstance should store io instance", () => {
       const routes = new Routes();
@@ -17,40 +32,97 @@ describe("Routes", () => {
   });
 
   describe("handler", () => {
-    const defaultParams = {
-      request: {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-        method: "",
-        body: {},
-      },
-      response: {
-        setHeader: jest.fn(),
-        writeHead: jest.fn(),
-        end: jest.fn(),
-      },
-      values: () => Object.values(defaultParams),
-    };
-
-    test("given an inexistent route it should choose default route", () => {
+    test("given an inexistent route it should choose default route", async () => {
       const routes = new Routes();
       const params = {
         ...defaultParams,
       };
 
       params.request.method = "inexistent";
-      routes.handler(...params.values());
+      await routes.handler(params.request, params.response);
 
       expect(params.response.end).toHaveBeenCalledWith("Hello World");
     });
 
-    test.todo("it should set any request with CORS enabled");
+    test("it should set any request with CORS enabled", async () => {
+      const routes = new Routes();
+      const params = {
+        ...defaultParams,
+      };
 
-    test.todo("given method OPTIONS it should choose options route");
+      params.request.method = "inexistent";
+      await routes.handler(params.request, params.response);
 
-    test.todo("given method POST it should choose post route");
+      expect(params.response.setHeader).toHaveBeenCalledWith(
+        "Access-Control-Allow-Origin",
+        "*"
+      );
+    });
 
-    test.todo("given method GET it should choose get route");
+    test("given method OPTIONS it should choose options route", async () => {
+      const routes = new Routes();
+      const params = {
+        ...defaultParams,
+      };
+
+      params.request.method = "OPTIONS";
+      await routes.handler(params.request, params.response);
+
+      expect(params.response.writeHead).toHaveBeenCalledWith(204);
+      expect(params.response.end).toHaveBeenCalled();
+    });
+
+    test("given method POST it should choose post route", async () => {
+      const routes = new Routes();
+      const params = {
+        ...defaultParams,
+      };
+
+      params.request.method = "POST";
+
+      await routes.handler(params.request, params.response);
+
+      expect(params.response.end).toHaveBeenCalled();
+    });
+
+    test("given method GET it should choose get route", async () => {
+      const routes = new Routes();
+      const params = {
+        ...defaultParams,
+      };
+
+      params.request.method = "GET";
+      await routes.handler(params.request, params.response);
+
+      expect(params.response.end).toHaveBeenCalled();
+    });
+  });
+
+  describe("get", () => {
+    test("given method GET it should list all files downloaded", async () => {
+      const routes = new Routes();
+      const params = {
+        ...defaultParams,
+      };
+      const filesStatusesMock = [
+        {
+          size: "25.9 kB",
+          lastModified: "2022-02-20T02:31:58.561Z",
+          owner: "carloshkruger",
+          file: "file.txt",
+        },
+      ];
+
+      jest
+        .spyOn(routes.fileHelper, "getFilesStatus")
+        .mockResolvedValue(filesStatusesMock);
+
+      params.request.method = "GET";
+      await routes.handler(params.request, params.response);
+      expect(params.response.writeHead).toHaveBeenCalledWith(200);
+      expect(params.response.end).toHaveBeenCalledWith(
+        JSON.stringify(filesStatusesMock)
+      );
+    });
   });
 });
